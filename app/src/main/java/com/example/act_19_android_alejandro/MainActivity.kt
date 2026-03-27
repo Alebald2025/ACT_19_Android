dcb86eef180034b70ad5bbde45287887b46adb24
AlejandroBaldeon <a24josbalesp@inspedralbes.cat>
2026-03-27 19:26:34 +0100

    Proyecto de android subido

---
+++ app/src/main/java/com/example/act_19_android_alejandro/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.act_19_android_alejandro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
